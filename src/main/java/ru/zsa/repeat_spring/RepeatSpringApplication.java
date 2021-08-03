3357b4f633d8358e5c9d4d1d61a4f9100bccade8
PaulFrere <paulfrere91@gmail.com>
2021-08-03 09:55:53 +0700

    New

---
+++ src/main/java/ru/zsa/repeat_spring/RepeatSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.zsa.repeat_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepeatSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zsa/repeat_spring/RepeatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zsa.repeat_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepeatSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
